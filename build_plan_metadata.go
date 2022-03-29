0e7007e04fadc5ec074df4c6de1c5d53d6cc0360
Joshua T. Casey <joshuatcasey@gmail.com>
2022-03-29 09:09:29 -0500

    Use buildpack api 0.7

--- build_plan_metadata.go
+++ build_plan_metadata.go
@@ -1,5 +1,7 @@
 package composer
 
+// BuildPlanMetadata is the buildpack specific data included in build plan
+// requirements.
 type BuildPlanMetadata struct {
 	VersionSource string `toml:"version-source"`
 	Version       string `toml:"version"`
