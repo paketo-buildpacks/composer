576b75f8e1c465cf515200cd064df7e4e8287cf2
Joshua Casey <joshuatcasey@gmail.com>
2022-11-09 16:21:58 -0600

    Use standard dependency workflow (#97)
    * Add new dependencies workflow
    
    * Add dependency retrieval step
    
    * Extract licenses from PHAR file
    
    * Add integration/unit/example tests for retrieval step
    
    * fix yaml lint

---
+++ dependency/retrieval/go.mod
@@ -0,0 +1,87 @@
+module github.com/paketo-buildpacks/pipenv/retrieval
+
+go 1.19
+
+// This is required because of a breaking change in a newer version
+replace github.com/ekzhu/minhash-lsh => github.com/ekzhu/minhash-lsh v0.0.0-20171225071031-5c06ee8586a1
+
+require (
+	github.com/Masterminds/semver/v3 v3.1.1
+	github.com/joshuatcasey/libdependency v0.24.0
+	github.com/onsi/gomega v1.22.1
+	github.com/paketo-buildpacks/occam v0.13.3
+	github.com/paketo-buildpacks/packit/v2 v2.6.1
+	github.com/sclevine/spec v1.4.0
+	golang.org/x/crypto v0.1.0
+)
+
+require (
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/ForestEckhardt/freezer v0.0.12 // indirect
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/Microsoft/hcsshim v0.9.4 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20221026131551-cf6655e29de4 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/anchore/packageurl-go v0.1.1-0.20220428202044-a072fa3cb6d7 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/cloudflare/circl v1.2.0 // indirect
+	github.com/containerd/cgroups v1.0.4 // indirect
+	github.com/containerd/containerd v1.6.8 // indirect
+	github.com/dgryski/go-minhash v0.0.0-20190315135803-ad340ca03076 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.18+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/ekzhu/minhash-lsh v0.0.0-20190924033628-faac2c6342f8 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.1 // indirect
+	github.com/go-enry/go-license-detector/v4 v4.3.0 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/go-git/go-git/v5 v5.4.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hhatto/gorst v0.0.0-20181029133204-ca9f730cac5b // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/jdkato/prose v1.2.1 // indirect
+	github.com/joshuatcasey/collections v0.5.0 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/moby/sys/mount v0.3.3 // indirect
+	github.com/moby/sys/mountinfo v0.6.2 // indirect
+	github.com/moby/term v0.0.0-20220808134915-39b0c02b01ae // indirect
+	github.com/montanaflynn/stats v0.6.6 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc2 // indirect
+	github.com/opencontainers/runc v1.1.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/shogo82148/go-shuffle v1.0.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/testcontainers/testcontainers-go v0.14.0 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
+	github.com/xanzy/ssh-agent v0.3.2 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/exp v0.0.0-20221031165847-c99f073a8326 // indirect
+	golang.org/x/mod v0.6.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/tools v0.2.0 // indirect
+	gonum.org/v1/gonum v0.12.0 // indirect
+	google.golang.org/genproto v0.0.0-20221010155953-15ba04fc1c0e // indirect
+	google.golang.org/grpc v1.50.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/neurosnap/sentences.v1 v1.0.7 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ dependency/retrieval/main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/joshuatcasey/libdependency/github"
+	"github.com/joshuatcasey/libdependency/retrieve"
+	"github.com/joshuatcasey/libdependency/versionology"
+	"github.com/paketo-buildpacks/packit/v2/cargo"
+	"github.com/paketo-buildpacks/packit/v2/fs"
+	"github.com/paketo-buildpacks/packit/v2/pexec"
+	"golang.org/x/crypto/openpgp"
+)
+
+func downloadToFile(url string) (filePath string, fileContents string, err error) {
+	response, err := http.DefaultClient.Get(url)
+	if err != nil {
+		return "", "", fmt.Errorf("could not get project metadata: %w", err)
+	}
+
+	if err != nil {
+		return "", "", err
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", "", fmt.Errorf("could not read response: %w", err)
+	}
+
+	tempDir, err := os.MkdirTemp("", "")
+	if err != nil {
+		return "", "", errors.New("could not create a temp dir")
+	}
+
+	filePath = filepath.Join(tempDir, filepath.Base(url))
+
+	err = os.WriteFile(filePath, body, os.ModePerm)
+	if err != nil {
+		return "", "", fmt.Errorf("could not write to file")
+	}
+
+	fileContents = string(body)
+
+	return
+}
+
+func verifyASC(signature, target, pgpKey string) error {
+	file, err := os.Open(target)
+	if err != nil {
+		return fmt.Errorf("could not open file: %w", err)
+	}
+	defer file.Close()
+
+	keyring, err := openpgp.ReadArmoredKeyRing(strings.NewReader(pgpKey))
+	if err != nil {
+		return err
+	}
+	signer, err := openpgp.CheckDetachedSignature(keyring, file, strings.NewReader(signature))
+	if signer == nil {
+		return fmt.Errorf("signature not accepted: %w", err)
+	}
+	return err
+}
+
+func GenerateMetadata(versionFetcher versionology.VersionFetcher) ([]versionology.Dependency, error) {
+	version := versionFetcher.Version().String()
+
+	uri := fmt.Sprintf("https://getcomposer.org/download/%s/composer.phar", version)
+
+	filePath, _, err := downloadToFile(uri)
+	if err != nil {
+		return nil, fmt.Errorf("could not download %s to file", uri)
+	}
+
+	upstreamChecksumUri := fmt.Sprintf("https://getcomposer.org/download/%s/composer.phar.sha256sum", version)
+	_, upstreamChecksum, err := downloadToFile(upstreamChecksumUri)
+	if err != nil {
+		return nil, fmt.Errorf("could not download %s to file", upstreamChecksumUri)
+	}
+
+	downloadedChecksum, err := fs.NewChecksumCalculator().Sum(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to calculate checksum of downloaded file: %w", err)
+	}
+
+	downloadedChecksum = fmt.Sprintf("%s  composer.phar", downloadedChecksum)
+
+	upstreamChecksum = strings.TrimSpace(upstreamChecksum)
+	if downloadedChecksum != "" && downloadedChecksum != upstreamChecksum {
+		return nil, fmt.Errorf("checksum mismatch. Downloaded SHA256 of '%s' should match expected checksum of '%s' from '%s'",
+			downloadedChecksum,
+			upstreamChecksum,
+			upstreamChecksumUri)
+	}
+
+	ascUri := fmt.Sprintf("https://getcomposer.org/download/%s/composer.phar.asc", version)
+
+	_, asc, err := downloadToFile(ascUri)
+	if err != nil {
+		return nil, fmt.Errorf("could not download %s to file", ascUri)
+	}
+
+	err = verifyASC(asc, filePath, composerPublicKey)
+	if err != nil {
+		return nil, fmt.Errorf("could not verify signature: %w", err)
+	}
+
+	sha256 := strings.Split(upstreamChecksum, " ")[0]
+	checksum := fmt.Sprintf("sha256:%s", sha256)
+
+	// Download the tarball and verify the checksum
+	// Download the tarball and verify the signature
+
+	configMetadataDependency := cargo.ConfigMetadataDependency{
+		CPE:            fmt.Sprintf("cpe:2.3:a:getcomposer:composer:%s:*:*:*:*:python:*:*", version),
+		Checksum:       checksum,
+		ID:             "composer",
+		Licenses:       retrieve.LookupLicenses(uri, PharDecompress),
+		Name:           "composer",
+		PURL:           retrieve.GeneratePURL("composer", version, sha256, uri),
+		Source:         uri,
+		SourceChecksum: checksum,
+		Stacks:         []string{"*"},
+		URI:            uri,
+		Version:        version,
+	}
+	return versionology.NewDependencyArray(configMetadataDependency, "NONE")
+}
+
+func PharDecompress(artifact io.Reader, destination string) error {
+	destinationFile := filepath.Join(destination, "composer.phar")
+
+	artifactBytes, err := io.ReadAll(artifact)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(destinationFile, artifactBytes, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	pharPath, err := exec.LookPath("phar")
+	if err != nil {
+		return err
+	}
+
+	phar := pexec.NewExecutable(pharPath)
+
+	var buffer *bytes.Buffer
+	buffer = bytes.NewBuffer(nil)
+
+	err = phar.Execute(pexec.Execution{
+		Dir: destination,
+		Args: []string{
+			"extract",
+			"-f", destinationFile,
+		},
+		Stdout: buffer,
+		Stderr: buffer,
+	})
+	if err != nil {
+		fmt.Println("Error running 'phar extract'")
+		fmt.Printf(buffer.String())
+		return err
+	}
+
+	return os.Remove(destinationFile)
+}
+
+func main() {
+	getAllVersions := github.GetAllVersions(os.Getenv("GIT_TOKEN"), "composer", "composer")
+
+	retrieve.NewMetadata("composer", getAllVersions, GenerateMetadata)
+}
